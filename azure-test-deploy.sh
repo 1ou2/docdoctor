1d07bef1ce90b2e82e6fd329b70f2532d4dd64be
gabriel pastor <github@1ou2.com>
2024-01-02 22:26:48 +0100

    test server

---
+++ azure-test-deploy.sh
@@ -0,0 +1,20 @@
+source azure.env
+
+# prepare packages
+FILES="requirements.txt tserver templates static .deployment startup.sh "
+rm -rf dist
+mkdir dist
+cp -r $FILES dist
+cd dist
+rm -f ./src.zip 2> /dev/null && zip src.zip $FILES
+
+echo "login using service principal"
+az login --service-principal --username $AZ_SERVICEPRINCIPAL_APPID --password $AZ_SERVICEPRINCIPAL_PASSWORD --tenant $AZ_TENANTID
+az account set --subscription $AZ_SUBSCRIPTION
+echo "****"
+echo "webapp deploy --resource-group $AZ_RESOURCEGROUP --name $AZ_WEBAPP_NAME --src-path src.zip"
+echo "***"
+az webapp deploy --resource-group $AZ_RESOURCEGROUP --name $AZ_WEBAPP_NAME --src-path "src.zip"
+echo "set startup file"
+az webapp config set --resource-group $AZ_RESOURCEGROUP --name $AZ_WEBAPP_NAME --startup-file "startup.sh"
+az webapp restart --name $AZ_WEBAPP_NAME --resource-group $AZ_RESOURCEGROUP
