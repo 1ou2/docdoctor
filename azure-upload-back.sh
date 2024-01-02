ac0996f57f3888fba03ed43ac381d31a6bf78510
Pastor Gabriel <gabriel.pastor@orange.com>
2024-01-02 15:23:50 +0100

    backend deployment

--- azure-upload-back.sh
+++ azure-upload-back.sh
@@ -7,3 +7,10 @@ mkdir dist
 cp $FILES dist
 cd dist
 rm -f ./src.zip 2> /dev/null && zip src.zip $FILES
+
+echo "login using service principal"
+az login --service-principal --username $AZ_SERVICEPRINCIPAL_APPID --password $AZ_SERVICEPRINCIPAL_PASSWORD --tenant $AZ_TENANTID
+az account set --subscription $AZ_SUBSCRIPTION
+
+az webapp deployment source config-zip --resource-group $AZ_RESOURCEGROUP --name $AZ_WEBAPP_NAME --src "src.zip"
+az webapp restart --name $AZ_WEBAPP_NAME --resource-group $AZ_RESOURCEGROUP
